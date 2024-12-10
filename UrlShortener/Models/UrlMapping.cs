8504f8abc00a2ef2a905d95ae334b3dd294547cc
Soilaguela <soilabenguela@gmail.com>
2024-12-10 14:32:43 +0000

    Adicionar arquivos de projeto.

---
+++ UrlShortener/Models/UrlMapping.cs
@@ -0,0 +1,12 @@
+﻿namespace UrlShortener.Models
+{
+    public class UrlMapping
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; }=string.Empty; //ou string?
+        public string ShortCode { get; set; } =string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int AccessCount { get; set; } // pk public int AccessCount { get; set; } = 0;
+    }
+}
